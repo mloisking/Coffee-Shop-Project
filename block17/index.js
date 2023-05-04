dd627facf2ba117d641b9aec50a2a1d7c799f829
mloisking <126358439+mloisking@users.noreply.github.com>
2023-05-04 17:18:19 -0400

    Added new branch

---
+++ block17/index.js
@@ -0,0 +1,37 @@
+//Put all coffee menu drinks in an array
+const coffeedrinks1 = ["cappucino", "espresso", "latte", "affogato", "macchiato","americano", 
+"icedcoffee","frappe", "cubanespresso"]
+
+//Put all drinks priced $5 and under in an array
+coffeedrinks.slice[0,4,"cubanespresso"]
+
+//Put all even price drinks in an array
+coffeedrinks.slice[0,3,4,6,7,"cubanespresso"]
+
+
+function coffeemenu(coffeedrinks, price) {
+    return coffeedrinks, price
+}
+let cappuccino="$8"
+let Espresso="$5"
+let latte="$5"
+let affogato="$8"
+let macchiato="6"
+let americano="$7"
+let icedcoffee="$6"
+let frappe="$8"
+let cubanespresso="$10"
+const coffeedrinkscost=("$8" +"5"+"5"+"8"+"6"+"7"+"$6"+"8"+"10")
+console.log("totalcost $63");
+
+//Put all seasonal drinks into an array
+//Expected output: array["cubanespresso"]
+const coffeedrinks = [cubanespresso]
+console.log(coffeemenu.slice(8));
+
+//Print an array with seasonal drinks with words "with imported beans"
+//Expected output: array["cubanexpresso with imported beans"]
+const coffeedrinks2 = ["cuban espresso with imported beans"]
+console.log(cubanespresso)
+
+<script src="coffee_data.js"></script>
